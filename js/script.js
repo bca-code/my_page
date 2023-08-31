106f94a0a22a6f93c0fe0d2121e9d8e6357ebd89
bca-dev <brandonca.dev+github@gmail.com>
2023-08-31 16:39:45 -0500

    able to re-show clippy again

--- js/script.js
+++ js/script.js
@@ -86,7 +86,7 @@ soundIcon.addEventListener("click", () => {
 
 const quotes = [
   "I use a Mac now...but that's besides the point.",
-  "I've always used computers as a consumer, but as I've gotten older I've grown to love how everything works.",
+  "I've always used computers as a consumer, but as I've gotten older I've grown to appreciate how everything works.",
   "<b>The Start Menu contains my Social Media:</b><br>1. GitHub<br>2. LinkedIn<br> 3. YouTube<br>4. ICODETHIS.",
   "<b>The Desktop Icons have additional information such as:</b><br>1. About Me<br>2. Projects<br>3. Resume<br>4. Contact Me",
   "Oh, that's self explanatory? Anyways..",
@@ -97,21 +97,31 @@ const quotes = [
 quotesIncrement = 0;
 const clippyContainer = document.querySelector(".clippy-container");
 const clippyTextBox = document.querySelector(".clippy-talk");
+const displayClippyBanner = document.querySelector(".resetClippy");
 clippySeen = localStorage.getItem("seenClippy") || false;
 
 if (clippySeen) {
   clippyContainer.style.display = "none";
+  displayClippyBanner.style.display = "block";
 }
 
 clippyContainer.addEventListener("click", () => {
   if (quotesIncrement >= quotes.length) {
-    clippyContainer.style.animation = "fade-in 2s reverse";
-    clippyContainer.style.pointerEvents = "none";
+    clippyContainer.style.display = "none";
     localStorage.setItem("seenClippy", true);
+    displayClippyBanner.style.display = "block";
   } else {
     clippyTextBox.innerHTML = quotes[quotesIncrement];
   }
   quotesIncrement++;
+});
+
+displayClippyBanner.addEventListener("click", () => {
+  quotesIncrement = 0;
+  clippyTextBox.innerHTML =
+    "Welcome to my Dev Page!<br><br>This desktop recreation represents where my fascination with computers started!";
+  clippyContainer.style.display = "block";
+  displayClippyBanner.style.display = "none";
 });
 
 // functions
