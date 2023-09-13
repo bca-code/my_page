46c732163a7da9ad78071d721f6231ca83ff2d22
bca-dev <brandonca.dev+github@gmail.com>
2023-09-13 12:22:44 -0500

    taskbar items easier to read. adjusted clippy messaging

--- js/clippy.js
+++ js/clippy.js
@@ -3,7 +3,6 @@
 const quotes = [
   "I've always used computers as a consumer, but as I've gotten older I've grown to appreciate how everything works.",
   "I'm currently seeking Junior Developer opportunities. Please contact me and let's get to know eachother!",
-  "You can mute sound effects below with the speaker icon.",
   "<b>The Start Menu contains my Social Media:</b><br>1. GitHub<br>2. LinkedIn<br> 3. YouTube<br>4. ICODETHIS.",
   "<b>The Desktop Icons have additional information such as:</b><br>1. About Me<br>2. Projects<br>3. Resume<br>4. Contact Me",
   "Enjoy my Developer Page!",
@@ -35,7 +34,7 @@ clippyContainer.addEventListener("click", () => {
 displayClippyBanner.addEventListener("click", () => {
   quotesIncrement = 0;
   clippyTextBox.innerHTML =
-    "Welcome to my Dev Page!<br><br>This desktop recreation represents where my fascination with computers started.<br><b>Click me to proceed.</b>";
+    "This desktop recreation represents where my fascination with computers started.<br><b>Click me to proceed.</b>";
   clippyContainer.style.display = "block";
   displayClippyBanner.style.display = "none";
 });
